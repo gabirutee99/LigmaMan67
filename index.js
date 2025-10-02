bdf986cd59432374cabf5a817f868124a87a936b
gabirutee99 <gpsilva2020pp@gmail.com>
2025-10-02 08:52:18 -0300

    enviando vs code

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+function mudar_cor_elemento(){
+    const paragrafo = document.querySelector('#paragrafo');
+
+    paragrafo.addEventListener('click', ()=> {
+        const elemento_paragrafo = document.querySelector('#paragrafo');
+
+        elemento_paragrafo.style.color = 'red';
+    
+    });
+
+}
+
 function mudar_conteudo_elemento() {
 
     document.getElementById("demo").innerHTML ="O clique está funcionando";
 }
+
+
+
