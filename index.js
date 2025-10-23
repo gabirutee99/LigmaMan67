fff86e550c55c7b5791a3fe5ff08a24642510fb2
gabirutee99 <gpsilva2020pp@gmail.com>
2025-10-23 08:45:30 -0300

    arquivos bla bla

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
+function mudar_texto_elemento() {
+
+  document.getElementById("demo").innerHTML = "O clique está funcionando"
+
+}
+
 function mudar_cor_elemento(){
-    const paragrafo = document.querySelector('#paragrafo');
 
-    paragrafo.addEventListener('click', ()=> {
-        const elemento_paragrafo = document.querySelector('#paragrafo');
+  const paragrafo = document.querySelector('#paragrafo');
 
-        elemento_paragrafo.style.color = 'red';
-    
-    });
+  paragrafo.addEventListener('click', ()=> {
+
+    const elemento_paragrafo = document.querySelector('#paragrafo');
+
+    elemento_paragrafo.style.color = 'red';
+
+  });
 
 }
 
-function mudar_conteudo_elemento() {
+function mudar_estilo_textual(){
 
-    document.getElementById("demo").innerHTML ="O clique está funcionando";
+  document.getElementById("elemento_strong").style.textDecorationLine = "line-through";
 }
-
-
-
